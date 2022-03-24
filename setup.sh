2da6ceecbe904788292714b5ae1fcc3b22eb75b7
Marcel Klehr <mklehr@gmx.net>
2022-03-24 13:27:48 +0100

    setup.sh: Update deploy branch to magpie-backend#master

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #! /bin/env bash
 rm -rf app
-git clone --depth=1 --branch docker https://github.com/magpie-ea/magpie-backend "./app"
+git clone --depth=1 --branch master https://github.com/magpie-ea/magpie-backend "./app"
